eaeebbdeea615455f0b7a36fc7da9482722d53a7
alakdeybabude <63889985+alakdeybabude@users.noreply.github.com>
2020-05-08 23:27:06 +0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,60 @@
+
+
+$('.owl-carousel').owlCarousel({
+  loop:true,
+  margin: 110,
+  
+  
+  autoplay:true,
+  responsive:{
+      0:{
+          items:1
+      },
+      600:{
+          items:3
+      },
+      1160:{
+          items:5
+      }
+  }
+});
+
+$('.count').counterUp({
+    delay: 10,
+    time: 1000
+});
+
+
+$('.about-').owlCarousel({
+    loop:false,
+    
+    
+    
+    autoplay:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1160:{
+            items:5
+        }
+    }
+})
+
+
+
+
+
+
+
+
+
+
+
+ 
+ 
+
+
